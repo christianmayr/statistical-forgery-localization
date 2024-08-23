45fc4252d64474bdd7506b9009e906925d075f78
Christian Rainer Mayr <christian.mayr@student.uibk.ac.at>
2024-08-23 15:47:45 +0200

    added formatting

--- adjpeg/primary_compression_estimation.py
+++ adjpeg/primary_compression_estimation.py
@@ -2,128 +2,119 @@ import numpy as np
 import matplotlib.pyplot as plt
 import jpeglib
 from jpeglib import DCTJPEG
+from adjpeg import utils
 
-def zz_index_8x8(index: int):
-    """
-    Calculates x and y index for zig-zag traversal for the i-th index for a 8x8 matrix
-    
-    Args:
-    - i: index
-    
-    Returns: x and y index for zig-zag traversal for the i-th index
-    """
-    
-    # calculate in which diagonal counted from the top left corner the number resides
-    ROW_STARTS = [1,2,4,7,11,16,22,29,37,44,50,55,59,62,64,65]
-    diagonal = 0
-    for i, row_start in enumerate(ROW_STARTS):
-        if not index >= row_start-1:
-            diagonal = i-1
-            break
-        
-    # calculate the index of the number within the diagonal row
-    diagnoal_index = index-(ROW_STARTS[diagonal]-1)
-        
-    # if the diagonal is going from left bottom to right top
-    if diagonal%2 == 0:
-        # calculate the diagonal start indices
-        diagonal_start_x = diagonal if diagonal < 8 else 7
-        diagonal_start_y = 0 if diagonal < 8 else diagonal-7
-        
-        # return the with the offset from the start indices
-        return (diagonal_start_x - diagnoal_index, diagonal_start_y + diagnoal_index)
-    
-    # if the diagonal is going from right top to left bottom
-    else:
-        # calculate the diagonal start indices
-        diagonal_start_x = 0 if diagonal < 8 else diagonal-7
-        diagonal_start_y = diagonal if diagonal < 8 else 7
-        
-        # return the with the offset from the start indices
-        return (diagonal_start_x + diagnoal_index, diagonal_start_y - diagnoal_index)
 
-def pce(img: jpeglib.DCTJPEG, dct_coefficient_range: range, max_dct_abs_value: int = 15, max_quantization_step = 100, __DEBUG__ = False):
+def primary_quantization_estimation(
+    img: jpeglib.DCTJPEG,
+    dct_coefficient_range: range,
+    max_dct_abs_value: int = 15,
+    max_quantization_step=100,
+    __DEBUG__=False,
+):
     """
     Estimate the first quantization matrix for a double-compressed image.
-    
+
     Args:
     - img: jpeglib image object
-    
+
     Returns: Quantization matrix
     """
-    
+
     # assure JPEG image has 64 coefficients per block
-    assert np.prod(img.Y.shape[2:]) == 64, "Number of DCT coefficients per block has to be 64"
-    
-    # Create cropped version to sample from    
+    assert (
+        np.prod(img.Y.shape[2:]) == 64
+    ), "Number of DCT coefficients per block has to be 64"
+
+    # Create cropped version to sample from
     img.write_dct("temp.jpeg")
     img_spatial = jpeglib.read_spatial("temp.jpeg")
-    
+
     shape_spatial = img_spatial.spatial.shape
-    assert shape_spatial[0] > 16 and shape_spatial[1] > 16, "Image is too small to be cropped and re-compressed"
-    
-    cropped_spatial = img_spatial.spatial[4:shape_spatial[0]-4,4:shape_spatial[1]-4,:]
+    assert (
+        shape_spatial[0] > 16 and shape_spatial[1] > 16
+    ), "Image is too small to be cropped and re-compressed"
+
+    cropped_spatial = img_spatial.spatial[
+        4 : shape_spatial[0] - 4, 4 : shape_spatial[1] - 4, :
+    ]
     img_spatial_cropped = img_spatial
     img_spatial_cropped.spatial = cropped_spatial
-    img_spatial_cropped.width-=8
-    img_spatial_cropped.height-=8
-    
+    img_spatial_cropped.width -= 8
+    img_spatial_cropped.height -= 8
+
     # Create img_0 (DCT coefficients sampled without compression)
     img_spatial_cropped.write_spatial("img_0.jpeg", qt=100)
     img_0 = jpeglib.read_dct("img_0.jpeg")
-    
+
     # Create img_q2 (DCT coefficients sampled with only second compression)
     # img_spatial_cropped.write_spatial("img_q2.jpeg", qt=img.qt)
     # img_q2 = jpeglib.read_dct("img_q2.jpeg")
-    
-    q1_result = np.full((8,8), np.nan)
-    
+
+    q1_result = np.full((8, 8), np.nan)
+    l_sum = 0
+
+    # TODO: Add expectation maximization
+
     for current_coefficient in dct_coefficient_range:
         # get coordinates for the DCT coefficient and the qt step
-        x, y = zz_index_8x8(current_coefficient)
-        
+        x, y = utils.zz_index_8x8(current_coefficient)
+
         # get DCT coefficient array from the cropped image
-        img_0_Y_dct = img_0.Y[:,:,x,y].copy()
+        img_0_Y_dct = img_0.Y[:, :, x, y].copy()
         img_0_Y_dct.sort()
-        
+
         # get DCT coefficient array from the original image and calculate the histogram
-        img_Y_dct_histogram, _ = np.histogram(img.Y[:,:,x,y], bins=np.arange(-max_dct_abs_value, max_dct_abs_value + 2) - 0.5)
-        
+        img_Y_dct_histogram, _ = np.histogram(
+            img.Y[:, :, x, y],
+            bins=np.arange(-max_dct_abs_value, max_dct_abs_value + 2) - 0.5,
+        )
+
         # get quantization step of the qt in the original image for the corresponding current_coefficient
-        q2: int = img.qt[0][zz_index_8x8(current_coefficient)]
-        
+        q2: int = img.qt[0][utils.zz_index_8x8(current_coefficient)]
+
         l_max = np.inf
-        for q1 in range(1, max_quantization_step+1):
-                        
+        for q1 in range(1, max_quantization_step + 1):
+
             ##### First quantization step #####
             # DCT coefficients in img_0 are quantized with value 1
             # Hence no multiplication needs to be performed to dequantize the coefficients
             img_c1_Y_dct: list[int] = np.round(np.divide(img_0_Y_dct, q1))
-            img_c1_Y_dct*=q1
+            img_c1_Y_dct *= q1
             # the result is dequantized DCT coefficients
-            
-            # TODO: Add truncation shifts
-            
+
             ##### Second quantization step #####
-            img_c2_Y_dct: list[int] = np.round(np.divide(img_c1_Y_dct, q2)) 
+            img_c2_Y_dct: list[int] = np.round(np.divide(img_c1_Y_dct, q2))
             # the result is quantized DCT coefficients
-            
+
             # calculate the histogram of the double quantized coefficients
-            img_c2_Y_dct_histogram, _ = np.histogram(img_c2_Y_dct, bins=np.arange(-max_dct_abs_value, max_dct_abs_value + 2) - 0.5)
-            
+            img_c2_Y_dct_histogram, _ = np.histogram(
+                img_c2_Y_dct,
+                bins=np.arange(-max_dct_abs_value, max_dct_abs_value + 2) - 0.5,
+            )
+
+            # TODO: Maybe add gaussean filter to img_c2_Y_dct_histogram to implement R/T error
+
             # compare to the histogram of the original image
             l = np.sum(np.abs(np.subtract(img_c2_Y_dct_histogram, img_Y_dct_histogram)))
-            
-            
+
             if l < l_max:
                 l_max = l
-                q1_result[zz_index_8x8(current_coefficient)]=q1
+                q1_result[utils.zz_index_8x8(current_coefficient)] = q1
                 w_histogram = img_c2_Y_dct_histogram
-         
+
+        l_sum += l_max
         if __DEBUG__:
-            print(f"DCT_coefficient: {current_coefficient}; (x,y): ({x},{y}); Q1: {q1_result[zz_index_8x8(current_coefficient)]}; L: {l_max}")
+            print(
+                f"DCT_coefficient: {current_coefficient}; (x,y): ({x},{y}); Q1: {q1_result[utils.zz_index_8x8(current_coefficient)]}; L: {l_max}"
+            )
             print("index\timg\tapprox")
             for i in range(len(w_histogram)):
-                print(f"{i-max_dct_abs_value}\t{img_Y_dct_histogram[i]}\t{w_histogram[i]}")
-        
+                print(
+                    f"{i-max_dct_abs_value}\t{img_Y_dct_histogram[i]}\t{w_histogram[i]}"
+                )
+
+    if __DEBUG__:
+        print(f"Error Sum: {l_sum}")
+
     return q1_result
--- tests/primary_compression_estimation_test.py
+++ tests/primary_compression_estimation_test.py
@@ -5,47 +5,64 @@ from adjpeg import primary_compression_estimation as pce
 import jpeglib
 import numpy.testing
 
-def load_image() -> jpeglib.DCTJPEG:
-    img = jpeglib.read_dct("./images/0_DC_50_0_80_0.jpeg")
+
+def load_image(image_path) -> jpeglib.DCTJPEG:
+    img = jpeglib.read_dct(image_path)
     return img
+
 
 class Benchmark(unittest.TestCase):
     def test_benchmark(self):
-        img = load_image()
-        
+        img = load_image("./images/0_DC_50_0_80_0.jpeg")
+
         with Profile() as profile:
-            result = pce.pce(img, range(0,15), max_dct_abs_value=15)
+            result = pce.primary_quantization_estimation(
+                img, range(0, 15), max_dct_abs_value=15
+            )
             print(f"\nOutput\n{result}")
-            (
-                Stats(profile)
-                .strip_dirs()
-                .sort_stats(SortKey.TIME)
-                .print_stats()
-            )  
+            (Stats(profile).strip_dirs().sort_stats(SortKey.TIME).print_stats())
+
 
 class BasicOperation(unittest.TestCase):
     def test_runs_without_exception(self):
-        img = load_image()
-        result = pce.pce(img, range(25,30), max_dct_abs_value=15, __DEBUG__ = True)
+        img = load_image("./images/double_compressed.jpg")
+        result = pce.primary_quantization_estimation(
+            img, range(0, 10), max_dct_abs_value=15, __DEBUG__=True
+        )
         print(f"\nOutput\n{result}")
-        
-        
-        
-class Utils(unittest.TestCase):
-    def test_zz_index_8x8(self):
-        test = numpy.full((8,8), numpy.nan)
-        for i in range(64):
-            test[pce.zz_index_8x8(i)]=i
-        
-        ref = numpy.array([
-            [ 0,  1,  5,  6, 14, 15, 27, 28],
-            [ 2,  4,  7, 13, 16, 26, 29, 42],
-            [ 3,  8, 12, 17, 25, 30, 41, 43],
-            [ 9, 11, 18, 24, 31, 40, 44, 53],
-            [10, 19, 23, 32, 39, 45, 52, 54],
-            [20, 22, 33, 38, 46, 51, 55, 60],
-            [21, 34, 37, 47, 50, 56, 59, 61],
-            [35, 36, 48, 49, 57, 58, 62, 63]
-            ])
-        
-        numpy.testing.assert_array_equal(test, ref)
+
+    def test_with_QF_75_80(self):
+        img = load_image("./images/0_DC_75_0_80_0.jpeg")
+        result = pce.primary_quantization_estimation(
+            img, range(0, 10), max_dct_abs_value=15, __DEBUG__=True
+        )
+        print(f"\nOutput\n{result}")
+
+    def test_with_QF_50_80(self):
+        img = load_image("./images/0_DC_50_0_80_0.jpeg")
+        result = pce.primary_quantization_estimation(
+            img, range(0, 32), max_dct_abs_value=15, __DEBUG__=True
+        )
+
+        ref = numpy.array(
+            [
+                [16, 11, 10, 16, 24, 40, 51, 61],
+                [12, 12, 14, 19, 26, 58, 60, 55],
+                [14, 13, 16, 24, 40, 57, 69, 56],
+                [14, 17, 22, 29, 51, 87, 80, 62],
+                [18, 22, 37, 56, 68, 109, 103, 77],
+                [24, 35, 55, 64, 81, 104, 113, 92],
+                [49, 64, 78, 87, 103, 121, 120, 101],
+                [72, 92, 95, 98, 112, 100, 103, 99],
+            ]
+        )
+
+        print(f"\nOutput\n{result}")
+
+        comparison_func = numpy.vectorize(
+            lambda x, y: numpy.nan if x == numpy.nan else x - y
+        )
+
+        correctness = comparison_func(result, ref)
+
+        print(f"\nCorrectness\n{correctness}")
